75e0d3c02a3c4b4d2272f277563af258a85c0998
Vinul Vinsura <vinulviksura16@gmail.com>
2024-05-16 20:19:03 +0530

    first commit

---
+++ src/test/java/com/example/librarymanagesystemborrowerservice/LibraryManageSystemBorrowerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.librarymanagesystemborrowerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManageSystemBorrowerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
